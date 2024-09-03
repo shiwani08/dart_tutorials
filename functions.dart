028d3641807b09140e3a24006f927c91fc1a43c1
geekordian <shiwanisoni29082002@gmail.com>
2024-09-03 15:27:02 +0530

    functions and constrcutor added

--- functions.dart
+++ functions.dart
@@ -1,7 +1,33 @@
 void main() {
   print(name("Shiwani", "Soni"));
+  User ob = User('shiwani', 22);
+  print(ob.name);
+  //parent class k contents are not printed
+  subUser ob2 = subUser('shiwani', 22);
+  ob2.publish();
+  //parent ka functions can be accessed by the child class 
+  ob2.login();
 }
 
 String name(String first, String last) {
   return '$first $last';
+}
+
+class User {
+  String name = "";
+  int age = 0;
+  User(String s, int a) {
+    name = s;
+    age = a;
+  }
+  void login() {
+    print("this is class User");
+  }
+}
+
+class subUser extends User {
+  subUser(String name, int age) : super(name, age);
+  void publish() {
+    print("this is child class subUser");
+  }
 }
---
+++ setmap.dart
@@ -0,0 +1,14 @@
+void main() {
+  const names = {'foo', 'bar', 1};
+  //can be used to change the type of the data
+  dynamic a = 90;
+  print(a);
+  a = "shiwani";
+  print(a);
+
+  List anything = ["any thing", 2, 20.98, 'can be added here'];
+  print(anything);
+
+  //if we define a datatype like List <Integer> only intergs can be added here--- this is a better practice
+   
+}
