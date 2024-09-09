210b41e9a7f40016f817aca0effadb3c44d15a77
geekordian <shiwanisoni29082002@gmail.com>
2024-09-09 14:57:48 +0530

    made changes to enumeration

--- enumeration.dart
+++ enumeration.dart
@@ -1,6 +1,23 @@
 void main() {
-  List<String> ob = Names.values.map((name) => name.toString()).toList();
+  List<String> ob = AnimalType.values.map((name) => name.toString()).toList();
   print(ob);
 }
 
-enum Names { first, last }
+enum AnimalType { cat, dog, rabbit }
+
+void test(AnimalType animalType) {
+  switch (animalType) {
+    case AnimalType.rabbit:
+      print("this is a rabbit");
+      break;
+    case AnimalType.cat:
+      print("this is a cat");
+      break;
+    case AnimalType.dog:
+      print("this is a dog");
+      break;
+
+      default:
+      print("this is the last block");
+  }
+}
